85381a896ee72814b8d2984e2a6472a489df749b
kokomin <hser0001@rangers.uwp.edu>
2019-04-05 15:27:59 -0500

    homework4 is done

---
+++ Homework4/MyCustomCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCustomCell.swift
+//  Homework4
+//
+//  Created by Eh Hser on 4/5/19.
+//  Copyright © 2019 Acer. All rights reserved.
+//
+
+import UIKit
+class MyCustomCell: UITableViewCell {
+    
+    @IBOutlet weak var myTitle: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+}
--- Homework4/ViewController.swift
+++ Homework4/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    let cities: [String] = ["Dubai", "London", "NewYork", "Paris", "Tokyo", "Zurich", "Munich"]
+    let cellReuseIdentifier = "cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.cities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:MyCustomCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! MyCustomCell
+        
+        // popolute each text and image view
+        cell.myTitle.text = self.cities[indexPath.row]
+        cell.myImage.image = UIImage(named: cities[indexPath.row] + ".jpg")
+        print("my text :  \(self.cities[indexPath.row])")
+        return cell
     }
 
 
